4c9372d8f493a0b3dce4eea99ffc249eba88d320
iL1nk <ilink@ua.fm>
2017-11-14 02:26:33 +0200

    initial setup

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by igor_linnyk on 14-11-2017
+ */
+console.log('123');
+
+import '../css/styles.css';
+
+import loaderImg from '../assets/loading.gif';
---
+++ src/js/weather-app/weather-app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by igor_linnyk on 14-11-2017
+ */
+console.log('weather app');
